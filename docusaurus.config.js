6b73ed1dd7b0a65a44e24d55bfcfb160fa5384aa
nengyuanzhang <13011132526@163.com>
2023-02-19 13:56:38 +0800

    added baidu site verification

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,15 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
+  headTags: [
+    {
+      tagName: 'meta',
+      attributes: {
+        name: 'baidu-site-verification',
+        content: 'codeva-8TUV7dDCxx',
+      },
+    },
+  ],
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
